e6b644374191b7f2b78552f4bfa8f9d97f4c1566
yosidakouki <kouki.gongon.0917@icloud.com>
2021-10-09 16:41:07 +0900

    さん付け

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          吉田<code></code>
+          吉田さん<code></code>
         </p>
         <a
           className="App-link"
